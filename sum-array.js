c43c49d74a3bf2f6b1d510bd8a600d84d4893e1e
Nick Barry <niko.barry@gmail.com>
2016-06-18 10:09:56 -0700

    Complete sum array problem
    Consider every possible contiguous segment of an array, and return the highest sum from all those contiguous segments.
    
    There is also a linear-time implementation of this (mine is quadratic, I believe), which I'll try implementing if this comes up again as a toy problem when I'm a senior.

---
+++ sum-array.js
@@ -0,0 +1,25 @@
+function sumArray (array) {
+  return Math.max(array.reduce((sum,num) => (sum + num)),
+    array.length > 1 ?
+      Math.max(sumArray(array.slice(1)), sumArray(array.slice(0,-1))) :
+      -Infinity
+  );
+}
+
+// MORE VERBOSE VERSION OF THE SAME LOGIC;
+//function sumArray (array) {
+//  if(array.length > 1){ // If array is only one element, we don't need to do anything recursive
+//    var sumEveryElement = sumEvery(array); // Check he sum of the entire array
+//    var sumSubslice1 = sumArray(array.slice(1)); // Recursively check the sum of the entire array, minus the 1st element
+//    var sumSubslice2 = sumArray(array.slice(0,-1)); // Recursively check the sum of the entire array, minus the last element
+//    return Math.max(sumEveryElement,sumSubslice1,sumSubslice2); // Return the largest
+//  }
+//
+//  return array[0]; // If array.length is 1, then just return the only element
+//
+//  function sumEvery(array){
+//    return array.reduce(function(sum,number){
+//      return sum + number;
+//    })
+//  }
+//}
