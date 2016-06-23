2995cf1c90170c0e15bf94ee63b0edb80fc85ed5
Nick Barry <niko.barry@gmail.com>
2016-06-23 08:15:02 -0700

    [WIP] Last Digit of a Huge Number (Code Wars kata) - need to preserve more sigdigs
    This passes almost all the tests, and in fact once actually passed ALL the tests. But it isn't complete yet, because the tests are somewhat randomized - the time I passed, there just didn't happen to be any tests that my function would fail. I'm committing here so I'll have a record of this implementation, since I'm about to restructure several lines of code in ways that won't be quick to reverse.

--- codewars/last-digit-of-a-huge-number.js
+++ codewars/last-digit-of-a-huge-number.js
@@ -1,11 +1,33 @@
+'use strict';
+
 var lastDigit = function(arr){
   if(!arr.length) {return 1;}
   if(1 === arr.length) {return arr[0] % 10;}
 
-  const lastDigit2 = function(str1, str2){
-    return +!+str2 || Math.pow(str1 % 10, str2 % 4 || 4) % 100;
+  const lastDigit2 = function(n1, n2){
+    if(0 === n2) {return 1;}
+    if(1 >= n1) {return n1;}
+    let n1ones = n1 % 10;
+    if(n1ones === 0) {return !n1 || 100;}
+    if(n1ones === 1) {
+      n1ones = (n1 % 100) > 1 ? n1 % 100 : 5;
+    }
+    let truncatedResult = Math.pow(n1ones, n2 % 4 || 4);
+    while(truncatedResult < 100){
+      truncatedResult *= Math.pow(n1ones,4);
+    }
+    return truncatedResult % 100;
   };
 
-  return arr.slice(0,-1).reverse().reduce((exp,base) => lastDigit2(base,exp),
-    arr[arr.length-1]) % 10;
+  return arr.slice(0,-1).reverse()
+      .reduce((exp,base) => {
+        //console.log(`Base: ${base}; Exponent: ${exp}`);
+        //console.log(lastDigit2(base,exp));
+        return lastDigit2(base,exp);
+      },
+      arr[arr.length-1]) % 10;
 };
+
+//console.log(lastDigit([45179,659733]));
+//console.log(lastDigit([534458,45179,659733])); // Expects 2; gets 8
+console.log(lastDigit([534458,99])); // Expects 2; gets 8
