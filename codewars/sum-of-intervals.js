c2e7ee4e1b57eae57224d73664dd49d467a843e4
Nick Barry <niko.barry@gmail.com>
2016-06-23 10:01:47 -0700

    Complete Sum of Intervals Code Wars kata

--- codewars/sum-of-intervals.js
+++ codewars/sum-of-intervals.js
@@ -1,19 +1,24 @@
 'use strict';
 
 function sumIntervals(intervals){
-  const newInts = [];
+  const combinedInts = [];
 
+  intervals.forEach(newInt => combineOverlappingIntervals(newInt,combinedInts));
 
+  return combinedInts.reduce((sum,int) => sum + int[1] - int[0], 0)
 
-  function findOverlappingIntervals(newInt,intervals){
-    const matches = [];
-
+  function combineOverlappingIntervals(newInt,intervals){
+    let combined = false;
     intervals.forEach((int,idx) => {
-      if(!(int[1] < newInt[0] || int[0] > int[1])){
-        matches.push(idx);
+      if(!(int[1] < newInt[0] || int[0] > newInt[1])){
+        const oldInt = intervals.splice(idx,idx+1)[0];
+        combined = intervals.push([Math.min(oldInt[0], newInt[0]), Math.max(oldInt[1], newInt[1])]);
       }
     });
-
-    return matches;
+    if(!combined){
+      intervals.push(newInt);
+    }
   }
 }
+
+console.log(sumIntervals([ [11,15],[6,10],[1,2] ]));
